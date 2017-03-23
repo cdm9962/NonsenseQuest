9332bc4f35bf319c5d5149252ec521a29870a14a
cdm9962 <cdm9962@g.rit.edu>
2017-03-22 22:06:22 -0400

    Refactored dirt path corner pngs.  Updaed DirtRoadSquare file names.

--- src/model/map/DirtRoadSquare.java
+++ src/model/map/DirtRoadSquare.java
@@ -12,13 +12,21 @@ public class DirtRoadSquare extends Square {
     public static char TOP_RIGHT_DIRT_ROAD_SQUARE_CHARACTER = 'w';
     public static char BOTTOM_RIGHT_DIRT_ROAD_SQUARE_CHARACTER = 'e';
     public static char BOTTOM_LEFT_DIRT_ROAD_SQUARE_CHARACTER = 'r';
+
+    public static String STRAIGHT_DIRT_ROAD_FILE = "file:dirtpathvert.png";
+    public static String TOP_LEFT_DIRT_ROAD_FILE = "file:dirtpathtopleft.png";
+    public static String TOP_RIGHT_DIRT_ROAD_FILE = "file:dirtpathtopright.png";
+    public static String BOTTOM_RIGHT_DIRT_ROAD_FILE = "file:dirtpathbottomright.png";
+    public static String BOTTOM_LEFT_DIRT_ROAD_FILE = "file:dirtpathbottomleft.png";
+
     public static String DIRT_ROAD_SQUARE_NAME = "Road";
     public static String DIRT_ROAD_SQUARE_DESCRIPTION = "A basic paved path.";
 
     /**
      * Dirt Road Square constructor.
      */
-    public DirtRoadSquare() {
-        super(DIRT_ROAD_SQUARE_NAME, DIRT_ROAD_SQUARE_DESCRIPTION, false);
+    public DirtRoadSquare(char character) {
+            super(DIRT_ROAD_SQUARE_NAME, DIRT_ROAD_SQUARE_DESCRIPTION, false);
+
     }
 }
