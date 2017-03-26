e9b6ff954af4ba89ec23107f3b48ba048728700b
cdm9962 <cdm9962@g.rit.edu>
2017-03-26 14:56:09 -0400

    Added splash screen and resized application to 16:9.

--- src/gui/GameInterface.java
+++ src/gui/GameInterface.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.*;
 import javafx.scene.paint.Paint;
@@ -30,17 +32,19 @@ public class GameInterface extends Application implements Observer {
     // The application stage
     private Stage primaryStage;
 
-    // Custom Font values
+    // Custom resources values
     public static final Font PIXEL_FONT_LARGE = Font.loadFont(GameInterface.class.getResource("/resources/images/pixelfont.ttf").toExternalForm(), 50);
     public static final Font PIXEL_FONT_MEDIUM = Font.loadFont(GameInterface.class.getResource("/resources/images/pixelfont.ttf").toExternalForm(), 30);
     public static final Font PIXEL_FONT_SMALL = Font.loadFont(GameInterface.class.getResource("/resources/images/pixelfont.ttf").toExternalForm(), 20);
+    public static final String SPLASH_FILE = "/resources/images/splash.png";
 
     // Constant values
     public static final String TITLE = "Nonsense Quest";
     public static final String INTRO_LABEL = "Welcome to Nonsense Quest!";
     public static final String INTRO_BUTTON_LABEL = "Start Adventure!";
     public static final int DEFAULT_INSETS = 50;
-    public static final int STAGE_SIZE = 1000;
+    public static final int STAGE_HEIGHT = 900;
+    public static final int STAGE_WIDTH = 1600;
 
     // JavaFx color values
     public static final String COLOR_GRAY = "-fx-background-color: GRAY;";
@@ -62,8 +66,8 @@ public class GameInterface extends Application implements Observer {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle(TITLE);
         primaryStage.setScene(new Scene(makeSplash()));
-        primaryStage.setHeight(STAGE_SIZE);
-        primaryStage.setWidth(STAGE_SIZE);
+        primaryStage.setHeight(STAGE_HEIGHT);
+        primaryStage.setWidth(STAGE_WIDTH);
         primaryStage.show();
         this.primaryStage = primaryStage;
     }
@@ -86,16 +90,12 @@ public class GameInterface extends Application implements Observer {
         // Create the border pane
         BorderPane border = new BorderPane();
         border.setPadding(new Insets(DEFAULT_INSETS));
-        border.setStyle(COLOR_GRAY);
 
-        // Create the top title label
-        Label label = new Label(INTRO_LABEL);
-        label.setPadding(new Insets(DEFAULT_INSETS));
-        label.setFont(PIXEL_FONT_LARGE);
-        label.setTextFill(Paint.valueOf(PAINT_BLUE));
-        label.setAlignment(Pos.CENTER);
-        Pane pane = new Pane(label);
-        border.setTop(pane);
+        // Create the background image
+        BackgroundImage splash= new BackgroundImage(new Image(getClass().getResource(SPLASH_FILE).toExternalForm(), 1600, 900, false, false),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        border.setBackground(new Background(splash));
 
         // Create the launch button
         GridPane grid = new GridPane();
@@ -109,7 +109,10 @@ public class GameInterface extends Application implements Observer {
         grid.add(start, 0, 0);
 
         // Adds button to the scene
-        border.setCenter(start);
+        AnchorPane anchor = new AnchorPane(grid);
+        anchor.setBottomAnchor(grid, 10.0);
+        anchor.setRightAnchor(grid, 618.0);
+        border.setBottom(anchor);
 
         return border;
     }
